5cd432af2de9126862e95586fbd2fd60eca73cf9
Ed1ezac <ed1ezac@gmail.com>
2023-05-26 20:00:41 +0200

    adds user control functionality

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'id_number', 'password',
     ];
 
     /**
@@ -37,4 +37,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /*public function payables(){
+        return $this->hasMany(Payable::class);
+    }*/
+
+    public function getInitialsAttribute(){
+        //call with usr->initials
+        $names = explode(' ', $this->name);
+        return $names[0][0].$names[count($names)-1][0];
+    }
 }
