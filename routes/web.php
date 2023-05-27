864a7b86ee231fcde081a3a331d019ebde6096fb
Ed1ezac <ed1ezac@gmail.com>
2023-05-27 02:08:52 +0200

    updates routes file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PayableController;
+use App\Http\Controllers\OrganizationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,10 +12,21 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'welcome']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+///Organizations
+Route::get('/organizations', [OrganizationController::class, 'list']);
+Route::get('/new/organization', [OrganizationController::class, 'creationForm']);
+Route::post('/create/organization', [OrganizationController::class, 'create'])->name('new_org');
+
+//Payables
+Route::get('/organization/{id?}/payables', [PayableController::class, 'forOrganization']);
+Route::get('/new/payable/', [PayableController::class, 'creationForm'])->name('new_payable');
+Route::post('/create/payable', [PayableController::class, 'create'])->name('create_payable');
