f05165a1df02ca8c20207f3d5b963bab2c3022f8
Ed1ezac <ed1ezac@gmail.com>
2023-05-27 02:08:00 +0200

    adds payables migration

---
+++ database/migrations/2023_05_26_214922_create_payables_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('organization_id');
+            $table->enum('type', ['fee', 'charge'])->default('fee');
+            $table->decimal('principal', $precision = 12, $scale = 2)->nullable();
+            $table->boolean('accrues')->deafault(0);
+            $table->string('accrues_every')->nullable();
+            $table->decimal('accrues_by', $precision = 9, $scale = 2)->nullable();
+            $table->boolean('recurs')->default(0);
+            $table->string('recurs_every')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payables');
+    }
+}
